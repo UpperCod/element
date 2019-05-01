f2377eeaed662d4a6ef5344a2a1da6ee100e52e7
Uppercod <mtrujillo@outlook.cl>
2019-04-30 23:49:01 -0400

    this package now does not export @atomico/core, now this is a peerDependency

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+/**
+ * @link https://www.npmjs.com/package/karma-rollup-preprocessor
+ */
+module.exports = function(config) {
+	config.set({
+		frameworks: ["jasmine"],
+		files: [
+			{
+				pattern: "test/**/*.test.js",
+				watched: true
+			}
+		],
+		preprocessors: {
+			"test/**/*.test.js": ["rollup"]
+		},
+		rollupPreprocessor: {
+			plugins: [
+				require("rollup-plugin-node-resolve")({
+					extensions: [".js", ".ts"]
+				}),
+				require("rollup-plugin-sucrase")({
+					production: true,
+					exclude: ["node_modules/**"],
+					jsxPragma: "h",
+					transforms: ["typescript", "jsx"]
+				})
+			],
+			output: {
+				format: "iife", // Helps prevent naming collisions.
+				name: "test", // Required for 'iife' format.
+				sourcemap: "inline" // Sensible for testing.
+			}
+		},
+		reporters: ["spec"],
+		port: 9876, // karma web server port
+		colors: true,
+		logLevel: config.LOG_ERROR,
+		browsers: ["ChromeHeadless"],
+		autoWatch: false,
+		// singleRun: false, // Karma captures browsers, runs the tests and exits
+		concurrency: Infinity
+	});
+};
